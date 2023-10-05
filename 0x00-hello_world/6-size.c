700d35deacb204351444b13e7a59f0b30cf2f886
Djimra NGARLEITA <dngarleita@gmail.com>
2023-10-05 16:29:13 -0700

    Chore: Low level programming, Hello World in c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void)
+{
+	char c;
+	int i;
+	long int lint;
+	long long int llint;
+	float f;
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(lint));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(llint));
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
+	return (0);
+}
