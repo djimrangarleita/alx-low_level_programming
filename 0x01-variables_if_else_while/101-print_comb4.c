22c2b44a4abbc1073156c56b39e67dce5040d215
Djimra NGARLEITA <dngarleita@gmail.com>
2023-10-07 02:29:44 +0300

    Chore: Print combination of 3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This function prints the smallest combination of digit numbers of base 10
+ * starting from 01
+ *
+ * Return: Always return 0 on success
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 1 + i; j < 10; j++)
+		{
+			for (k = j + 1; k < 10; k++)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar('0' + k);
+				if (j != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
