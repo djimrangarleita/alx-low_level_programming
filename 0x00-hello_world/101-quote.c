a9119ff00882f7c1456c9ae534d8fef3effe4f1d
Djimra NGARLEITA <dngarleita@gmail.com>
2023-10-05 18:10:09 -0700

    Chore: Do task 8, optional

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Print a message on standard error
+ *
+ * Return: return 0 on success
+ */
+int main(void)
+{
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
