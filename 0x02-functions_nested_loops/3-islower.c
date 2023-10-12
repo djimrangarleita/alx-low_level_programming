f0f67447104b13b640d1ba0fd401e1e7ed373db6
Djimra NGARLEITA <dngarleita@gmail.com>
2023-10-12 04:15:29 +0300

    Chore: Do task 3, islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * _islower - Print alphabets from a to z followed by a new line
+ */
+int _islower(int c)
+{
+	return (islower(c));
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,9 +2,11 @@
 #define HEADER_FILE
 
 #include <string.h>
+#include <ctype.h>
 
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
 
 #endif
