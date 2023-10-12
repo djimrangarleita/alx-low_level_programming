f20d12730c694a533bf7dd729dec7583b8282d2e
Djimra NGARLEITA <dngarleita@gmail.com>
2023-10-12 04:32:56 +0300

    Fix: Return type

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,14 @@
 
 /**
  * _islower - Print alphabets from a to z followed by a new line
+ * @c: int param, the char to be tested
+ * Return: 1 if char is lower, 0 if not
  */
 int _islower(int c)
 {
-	return (islower(c));
+	if (islower(c))
+	{
+		return (1);
+	}
+	return (0);
 }
--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -15,6 +15,8 @@ int main(void)
     	_putchar(r + '0');
     	r = _islower(108);
     	_putchar(r + '0');
+    	r = _islower('c');
+    	_putchar(r + '0');
     	_putchar('\n');
 	return (0);
 }
