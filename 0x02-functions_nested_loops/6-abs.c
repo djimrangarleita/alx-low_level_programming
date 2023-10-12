a318b1201b51b5f6a7edda004c5fda2ce776ec8e
Djimra NGARLEITA <dngarleita@gmail.com>
2023-10-12 05:21:56 +0300

    Fix: Correct betty doc style

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isalpha - computes the absolute value of an integer
+ * _abs - computes the absolute value of an integer
  * @d: int param, the int to be computed
  * Return: The absolute value
  */
