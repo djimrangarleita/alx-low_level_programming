825bc50f203af542a1b8636e7fbb7c3ba922a45c
Djimra NGARLEITA <dngarleita@gmail.com>
2023-10-12 19:23:43 +0300

    Fix: Use _putchar only 3 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,15 +13,11 @@ void more_numbers(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			if (j < 10)
-			{
-				_putchar(j + '0');
-			}
-			else
+			if (j > 9)
 			{
 				_putchar(j / 10 + '0');
-				_putchar(j % 10 + '0');
 			}
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
