0cf4f17d4bfe84c688f4b064cc2ed1bde035fd9f
Djimra NGARLEITA <dngarleita@gmail.com>
2023-10-13 05:57:50 +0300

    Chore: Print table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_times_table - Print times table
+ * @n: Table to print
+ *
+ */
+void print_times_table(int n)
+{
+	int i;
+	int j;
+	int val;
+
+	if (n < 15 && n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			val = 0;
+			_putchar(0 + '0');
+			_putchar(',');
+			for (j = 0; j < n; j++)
+			{
+				val += i;
+				if (val > n)
+				{
+					_putchar(' ');
+					_putchar(val / 10 + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(val % 10 + '0');
+				if (j < n - 1)
+				{
+					_putchar(',');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
