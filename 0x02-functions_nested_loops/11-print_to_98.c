96e8032b2fc1a10c4b8c66f44f7b3358bc758184
Djimra NGARLEITA <dngarleita@gmail.com>
2023-10-13 05:25:42 +0300

    Fix: Use printf instead of putchar, betty

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,41 +8,26 @@
  */
 void print_to_98(int n)
 {
-	int inc = 1;
-	/* int tmp; */
-
-	if (n > 98)
-		inc = -1;
-	do {
-		printf("%d, ", n);
-		/*
-		if (n < 0)
+	if (n > 99)
+	{
+		while (n >= 98)
 		{
-			_putchar('-');
-			tmp = abs(n);
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+			n--;
 		}
-		else
-		{
-			tmp = n;
-		}
-		if (tmp < 10)
-		{
-			_putchar(tmp + '0');
-		}
-		else
-		{
-			_putchar(tmp / 10 + '0');
-			_putchar(tmp % 10 + '0');
-		}
-		if (n != 98)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
-		*/
-		n += inc;
 		printf("\n");
-	} while (n < 99);
-	_putchar('\n');
-	_putchar('\n');
+	}
+	else
+	{
+		while (n < 99)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+			n++;
+		}
+		printf("\n");
+	}
 }
