7f27cc81415bde4d2030639e2df2526980e1e4d3
Djimra NGARLEITA <dngarleita@gmail.com>
2023-10-13 04:29:13 +0300

    Fix: Print times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,17 +8,24 @@ void times_table(void)
 {
 	int i;
 	int j;
-	int val;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		int val = 0;
+		_putchar(0 + '0');
+		_putchar(',');
+		_putchar(' ');
+		for (j = 0; j < 9; j++)
 		{
-			val = i + j;
-			_putchar(val / 10 + '0');
+			val += i;
+			if (val > 9)
+				_putchar(val / 10 + '0');
 			_putchar(val % 10 + '0');
-			_putchar(',');
-			_putchar(' ');
+			if (j < 8)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
