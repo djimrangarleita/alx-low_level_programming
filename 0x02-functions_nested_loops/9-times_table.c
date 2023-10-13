d6c4f264bbfc516e2cd143881bec49fa30ac1811
Djimra NGARLEITA <dngarleita@gmail.com>
2023-10-13 04:41:35 +0300

    Fix: Format output, betty

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,30 +1,37 @@
 #include "main.h"
 
 /**
- * jack_bauer - Print every minute of jack bauer
+ * times_table - Print times table
  *
  */
 void times_table(void)
 {
 	int i;
 	int j;
+	int val;
 
 	for (i = 0; i < 10; i++)
 	{
-		int val = 0;
+		val = 0;
 		_putchar(0 + '0');
 		_putchar(',');
-		_putchar(' ');
 		for (j = 0; j < 9; j++)
 		{
 			val += i;
 			if (val > 9)
+			{
+				_putchar(' ');
 				_putchar(val / 10 + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(' ');
+			}
 			_putchar(val % 10 + '0');
 			if (j < 8)
 			{
 				_putchar(',');
-				_putchar(' ');
 			}
 		}
 		_putchar('\n');
