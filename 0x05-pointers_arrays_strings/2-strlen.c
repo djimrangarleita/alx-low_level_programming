13ca9318b129209381da2015b0dd32eec2cd5525
Djimra NGARLEITA <dngarleita@gmail.com>
2023-10-16 22:39:32 +0300

    Chore: Return length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include <string.h>
+
+/**
+ * _strlen - Takes a string param and return its length
+ * @s: The string who's length must be returned
+ * Return: Int, length of the string s
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
