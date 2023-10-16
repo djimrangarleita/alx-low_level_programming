90a8d9004f4376f457602c35bf232ea43576be68
Djimra NGARLEITA <dngarleita@gmail.com>
2023-10-17 02:42:15 +0300

    Chore: Print n elements of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * print_array - Print second half of a string
+ * @a: Array to print
+ * @n: Number of elements to print
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i < n - 1)
+			printf("%d, ", a[i]);
+		else
+			printf("%d\n", a[i]);
+	}
+}
