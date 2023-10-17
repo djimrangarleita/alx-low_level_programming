320d46bfc77886fefddf9d91f029ec56e30d924a
Djimra NGARLEITA <dngarleita@gmail.com>
2023-10-17 03:15:40 +0300

    Chore: Copy string from src to dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include <string.h>
+
+/**
+ * _strcpy - Copy a string from src to dest
+ * @src: String source to be copied to dest
+ * @dest: Destination where to paste src
+ * Return: Pointer to dest string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
