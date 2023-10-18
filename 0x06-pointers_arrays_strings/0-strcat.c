263d156efcbab2807f886a87764176aa22f78ef3
Djimra NGARLEITA <dngarleita@gmail.com>
2023-10-18 07:39:45 +0300

    Chore: Concatenate 2 strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include <string.h>
+
+/**
+ * _strcat - Concatenate 2 strings
+ * @dest: Destination string
+ * @src: Source string
+ * Return: Pointer to concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
