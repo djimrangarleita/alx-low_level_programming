6d7dc037a7c32591eb3ac26421c108ce1a68f732
Djimra NGARLEITA <dngarleita@gmail.com>
2023-10-19 06:28:59 +0300

    Chore: Transform string to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
 /**
  * string_toupper - COnvert a string to uppercase
- * @s: Pointer to string
+ * @s: String to transform
+ * Return: Pointer to string
  */
 char *string_toupper(char *s)
 {
 	int i = 0;
+
 	while (s[i] != '\0')
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
