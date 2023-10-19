f530712942982681c29c8dc82b3645ae138c460a
Djimra NGARLEITA <dngarleita@gmail.com>
2023-10-19 15:58:45 +0300

    Chore: Capitalize string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include <string.h>
+
+/**
+ * cap_string - Capitalize string
+ * @s: String to capitalize
+ * Return: Pointer to string
+ */
+char *cap_string(char *s)
+{
+	int i = 0, j;
+	char separator[] = " \n	,;.!?\"(){}";
+	int seplen = strlen(separator);
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; j < seplen; j++)
+		{
+			if (s[i - 1] == separator[j] && s[i] >= 'a' && s[i] <= 'z')
+				s[i] = 'A' + (s[i] - 'a');
+		}
+		i++;
+	}
+
+	return (s);
+}
