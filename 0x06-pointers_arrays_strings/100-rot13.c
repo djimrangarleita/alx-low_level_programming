e80402183763ec73aa119843e8165a28633d79d0
Djimra NGARLEITA <dngarleita@gmail.com>
2023-10-20 05:46:04 +0300

    ---

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * rot13 - Encode string using root13
+ * @s: String to be encoded
+ * Return: Pointer to string
+ */
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	int len;
+	char comparator[52] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char equiv[52] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	len = strlen(s);
+	for (i = 0; i < len; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == comparator[j])
+				s[i] = equiv[j];
+		}
+	}
+
+	return (s);
+}
--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,7 +15,7 @@ char *cap_string(char *s)
 	{
 		for (j = 0; j < seplen; j++)
 		{
-			if (s[i] != '\0' && s[i - 1] == separator[j] && s[i] >=
+			if (s[i - 1] == separator[j] && s[i] >=
 					'a' && s[i] <= 'z')
 				s[i] = 'A' + (s[i] - 'a');
 		}
