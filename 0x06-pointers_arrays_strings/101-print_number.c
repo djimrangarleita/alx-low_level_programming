b20af1945938f0e5ae706e4c0c5a27218bfa7b5a
Djimra NGARLEITA <dngarleita@gmail.com>
2023-10-21 02:11:54 +0300

    Fix: Print long, intmin

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,12 +7,22 @@
  */
 void print_number(int n)
 {
+	int lastdigit;
+	int sign = 1;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		lastdigit = n % 10;
+		n /= -10;
+		sign = -1;
 	}
+
 	if (n / 10)
 		print_number(n / 10);
 	_putchar(n % 10 + '0');
+
+	if (sign < 0)
+		_putchar('0' - lastdigit);
+
 }
