3ad212db83b8daa77d9d517c99329197de687d44
Djimra NGARLEITA <dngarleita@gmail.com>
2023-10-21 04:14:04 +0300

    Chore: Create files

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,13 @@
+#include <string.h>
+
+/**
+ * infinite_add - Add infinite numbers
+ * @n1: Number 1
+ * @n2: Number 2
+ * r: Buffer to store
+ * size_r: Size of the buffer
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	return (r);
+}
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,9 @@
+/**
+ * print_buffer - 
+ * @b: buffer pointed to
+ * @size: size of the buffer
+ */
+void print_buffer(char *b, int size)
+{
+	
+}
