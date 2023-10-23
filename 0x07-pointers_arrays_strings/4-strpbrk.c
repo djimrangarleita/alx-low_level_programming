5d3215e286840deda07ac5bdfb36a6913f2d9066
Djimra NGARLEITA <dngarleita@gmail.com>
2023-10-24 02:00:13 +0300

    Chore: Compare to strings

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+/**
+ * _strpbrk - Check if any char in s match any char in accept
+ * @s: Initial string
+ * @accept: Accepted to search
+ * Return: Pointer to the first occurence of match
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		int j = 0;
+
+		while (s[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+			j++;
+		}
+		i++;
+	}
+
+	return (0);
+}
