a4f61769d2eb5cc7720bc407b39d9fc8a738c243
Djimra NGARLEITA <dngarleita@gmail.com>
2023-10-24 01:51:11 +0300

    Chore: Get length of a prefix substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
  * _strspn - Get the length of a prefix substring
  * @s: Initial string
@@ -6,5 +8,29 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	
+	int i = 0;
+	unsigned int len = 0;
+
+	while (s[i] != '\0')
+	{
+		int j = 0;
+		int foundChar = 0;
+
+		while (s[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				/* printf("Found %c\n", accept[j]); */
+				foundChar = 1;
+				len++;
+				break;
+			}
+			j++;
+		}
+		if (foundChar == 0)
+			break;
+		i++;
+	}
+
+	return (len);
 }
