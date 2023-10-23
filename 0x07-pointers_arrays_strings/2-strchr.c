1be428dee6373d75a23e794b77ea60bd7e41ec89
Djimra NGARLEITA <dngarleita@gmail.com>
2023-10-23 23:14:40 +0300

    Chore: Locate a char in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+/**
+ * _strchr - Locate a char in a string
+ * @s: String to search from
+ * @c: Char to locate
+ * Return: Pointer to first occurence of c in s, or null if c not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return (&s[i]);
+		i++;
+	}
+
+	return (0);
+}
