88d108a8eeb62b2c1897ef50545cc3b18464da40
Djimra NGARLEITA <dngarleita@gmail.com>
2023-10-24 03:14:23 +0300

    Chore: Print chessboard

---
+++ 0x07-pointers_arrays_strings/6-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Print a chessboard
+ * @a: Array of chessboard dimension
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+			if (j == 7)
+				_putchar('\n');
+		}
+	}
+}
