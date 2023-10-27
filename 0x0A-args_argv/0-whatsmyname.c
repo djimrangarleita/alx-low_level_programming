4a26f3db1e2fccfcdc950c449695ae77855afe15
Djimra NGARLEITA <dngarleita@gmail.com>
2023-10-28 02:09:44 +0300

    Chore: Print program name

---
+++ 0x0A-args_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, driver
+ * @argc: The count of arguments passed to main
+ * @argv: The vector of argv, pointer to array of str
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
