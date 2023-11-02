d846ce4436c481e8fd8b119412e9601d18139bf6
Djimra NGARLEITA <dngarleita@gmail.com>
2023-11-03 02:23:50 +0300

    fix: print NULL as empty str

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
 
 char *_strncat(char *s1, char *s2, int max_i);
 
@@ -22,14 +21,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 && *s2)
 		s2len = strlen(s2);
 
-	printf("s1: %d and s2: %d\n", s1len, s2len);
 	if (s2len < (int)n)
 		max_i = s1len + s2len;
 	else
 		max_i = s1len + n;
 
 	scat = _strncat(s1, s2, max_i);
-	if (!scat || !*scat)
+	if (!scat)
 		return (NULL);
 
 	return (scat);
@@ -47,19 +45,17 @@ char *_strncat(char *s1, char *s2, int max_i)
 	int i, j;
 	char *scat;
 
-	printf("max i is %d\n", max_i);
 	scat = malloc((max_i + 1) * sizeof(*scat));
 	if (!scat)
 		return (NULL);
 
-	printf("Passed if and max i is %d\n", max_i);
 	i = 0;
-	while (max_i > 0 && s1[i] != '\0')
+	while (s1 && s1[i] != '\0')
 	{
 		scat[i] = s1[i];
 		i++;
 	}
-	printf("Passed first while and max i is %d\n", max_i);
+
 	j = 0;
 	while (i < max_i)
 	{
@@ -68,7 +64,6 @@ char *_strncat(char *s1, char *s2, int max_i)
 		j++;
 	}
 
-	printf("Passed second while and max i is %d and i: %i\n", max_i, i);
 	scat[i] = '\0';
 
 	return (scat);
