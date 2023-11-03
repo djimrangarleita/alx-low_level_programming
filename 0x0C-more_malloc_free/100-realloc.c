ed0bd074606fc9c43691cc5b9c3ba1eacb254b81
Djimra NGARLEITA <dngarleita@gmail.com>
2023-11-03 05:58:08 +0300

    fix: betty

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,14 +1,15 @@
 #include <stdlib.h>
 
 /**
- * _calloc - allocate mem to an array of @nmemb elt of size @size each
- * @nmemb: elt of the array
- * @size: size of each array elt
+ * _realloc - allocate mem to an array of @nmemb elt of size @size each
+ * @old_size: old size of ptr
+ * @newsize: size of new mem block
+ * @ptr: pointer to old block
  * Return: void pointer or NULL
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int newsize)
 {
-	if(newsize == 0 && ptr != NULL)
+	if (newsize == 0 && ptr != NULL)
 	{
 		free(ptr);
 
