bee361c27e5d807945e1dc7ab4456933dead5d2e
Djimra NGARLEITA <dngarleita@gmail.com>
2023-11-07 05:48:29 +0300

    fix: return NULL if the fn fails

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -33,6 +33,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 		else
 			return (NULL);
 	}
+	else
+	{
+		return (NULL);
+	}
 	if (lowner)
 	{
 		newdog->owner = malloc(lowner * sizeof(char));
