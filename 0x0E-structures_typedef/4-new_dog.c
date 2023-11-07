af277fedcaa23442a63ff138950a39d88a56b719
Djimra NGARLEITA <dngarleita@gmail.com>
2023-11-07 03:50:28 +0300

    fix: init dog and create new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,23 +1,31 @@
 #include <stdlib.h>
 #include "dog.h"
+#include <string.h>
 
 /**
  * new_dog - create a new dog
  * @name: name of the new dog
  * @age: age of the new dog
  * @owner: owner of the new dog
+ * Return: pointer to newdog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog;
-	
+
+	char *dname = malloc(sizeof(char) * strlen(name));
+	char *oname = malloc(sizeof(char) * strlen(owner));
 	newdog = malloc(sizeof(dog_t));
-	if (!newdog)
+
+	if (!dname || !oname || !newdog)
 		return (NULL);
 
-	newdog->name = name;
+	dname = name;
+	oname = owner;
+
+	newdog->name = dname;
 	newdog->age = age;
-	newdog->owner = owner;
+	newdog->owner = oname;
 
 	return (newdog);
 }
