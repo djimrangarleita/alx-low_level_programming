38258c5093f4425f6f8f948c25f758857b15364c
Djimra NGARLEITA <dngarleita@gmail.com>
2023-11-08 23:44:20 +0300

    chore: print name from callback

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef FPOINTER_H
 #define FPOINTER_H
 
+void _putchar(char *name);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
