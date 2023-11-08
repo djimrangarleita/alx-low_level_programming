281ea3616e619d1b164b86ff419234c3f40d417b
Djimra NGARLEITA <dngarleita@gmail.com>
2023-11-08 23:43:24 +0300

    chore: print name from callback

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,9 @@
+/**
+ * print_name - print a name
+ * @name: string pointer to the name to be printed
+ * @f: function pointer, the function that will print name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
