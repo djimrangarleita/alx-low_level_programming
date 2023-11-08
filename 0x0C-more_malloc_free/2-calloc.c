9538cd8e7bfc96d65779b845d3b10a6b5cd123d6
Djimra NGARLEITA <dngarleita@gmail.com>
2023-11-08 15:08:38 +0300

    fix: calloc and init with null

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,23 +10,18 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int i;
-	char *nptr;
 	void *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	nptr = malloc(nmemb * size);
 	ptr = malloc(nmemb * size);
 
-	if (nptr == NULL || ptr == NULL)
+	if (ptr == NULL)
 		return (NULL);
 
 	for (i = 0; i < (int)nmemb; i++)
-		nptr[i] = '\0';
-
-	memcpy(ptr, nptr, nmemb);
-	free(nptr);
+		*((char *)ptr + i) = '\0';
 
 	return (ptr);
 }
