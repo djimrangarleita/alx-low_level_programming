61e6060f621ecf6d02790307bd4c8df7b1950caa
Djimra NGARLEITA <dngarleita@gmail.com>
2023-11-09 00:20:06 +0300

    fix: array can be null

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,6 +2,7 @@
 
 /**
  * array_iterator - interate over each elt of array
+ * @array: array of @size elt
  * @size: size of the array
  * @action: function ptr, the function to execute on each elt
  */
@@ -9,8 +10,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	for (i = 0; i < size; i++)
+	if (array != NULL)
 	{
-		action(array[i]);
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
 	}
 }
