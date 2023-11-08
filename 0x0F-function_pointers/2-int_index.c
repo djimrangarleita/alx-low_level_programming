6c037d1600d9d62ecf03b53451ebdfad755cabab
Djimra NGARLEITA <dngarleita@gmail.com>
2023-11-09 00:36:26 +0300

    chore: compare 2 elts, fix array_iterator size

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+/**
+ * int_index - search for an int
+ * @array: array of int elt where to seach
+ * @size: size of the array
+ * @cmp: func ptr, function that make the comparison
+ * Return: index of the first match occurence
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	if (array)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+				return (i);
+		}
+	}
+
+	return (-1);
+}
