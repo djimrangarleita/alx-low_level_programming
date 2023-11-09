e4134f4ba6bb5e0d75da2d902297bdbc96e680ce
Djimra NGARLEITA <dngarleita@gmail.com>
2023-11-09 06:53:27 +0300

    fix: write a strcpy function + free mem

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,8 @@
 #include "dog.h"
 #include <string.h>
 
+char *_strcpy(char *dest, char *src);
+
 /**
  * new_dog - create a new dog
  * @name: name of the new dog
@@ -12,24 +14,55 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog;
-	char *dname, *oname;
 
-	if (name && owner)
-	{
-		dname = malloc(sizeof(char) * strlen(name));
-		oname = malloc(sizeof(char) * strlen(owner));
-	}
-	newdog = malloc(sizeof(dog_t));
-
-	if (!dname || !oname || !newdog)
+	if (!name || !owner)
 		return (NULL);
 
-	dname = name;
-	oname = owner;
+	newdog = malloc(sizeof(dog_t));
 
-	newdog->name = dname;
+	if (!newdog)
+		return (NULL);
+
+	newdog->name = malloc(sizeof(char) * strlen(name) + 1);
+	if (!newdog->name)
+	{
+		free(newdog);
+		return (NULL);
+	}
+
+	newdog->owner = malloc(sizeof(char) * strlen(owner) + 1);
+	if (!newdog->owner)
+	{
+		free(newdog->name);
+		free(newdog);
+		return (NULL);
+	}
+
 	newdog->age = age;
-	newdog->owner = oname;
+	_strcpy(newdog->name, name);
+	_strcpy(newdog->owner, owner);
 
 	return (newdog);
+}
+
+/**
+ * _strcpy - copy string src to dest
+ * @dest: pointer to dest location, array of char
+ * @src: pointer to src, array chars
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
 }
