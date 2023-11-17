c08677538045d0decb9a98009ff93610b48efbc7
Djimra NGARLEITA <dngarleita@gmail.com>
2023-11-17 08:09:00 +0300

    chore: execute function before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,5 @@ void first_exec(void) __attribute__((constructor));
  */
 void first_exec(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my
-			back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
