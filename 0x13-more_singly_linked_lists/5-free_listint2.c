5569f8d824ca9fdf171559eb21d5b038292ca056
Djimra NGARLEITA <dngarleita@gmail.com>
2023-11-21 06:39:30 +0300

    chore: free mem 2, set ptr to null

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - add new node at the beginning of a list
+ * @head: current head of the list
+ * Return: The address of the new elt or null
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	while (*head != NULL)
+	{
+		ptr = (*head)->next;
+		free(*head);
+		*head = ptr;
+	}
+}
