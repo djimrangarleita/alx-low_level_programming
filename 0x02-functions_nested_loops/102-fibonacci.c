05a9fcc6beb6ae563d09ad20dab651dac07b7773
Djimra NGARLEITA <dngarleita@gmail.com>
2023-11-22 07:29:56 +0300

    fix: correct format specifier

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-	int i, n1 = 0, n2 = 1;
-	unsigned int fib = 0;
+	int i;
+	long int n1 = 0, n2 = 1, fib = 0;
 
 	for (i = 0; i < 50; i++)
 	{
 		fib = n1 + n2;
-		printf("%u", fib);
+		printf("%ld", fib);
 		n1 = n2;
 		n2 = fib;
 		if (i < 49)
