fb4835289aef15e2ae7f9c22cb66f0aaec4acc36
Djimra NGARLEITA <dngarleita@gmail.com>
2023-11-24 16:57:29 +0300

    fix: index shouldn't exceed 31

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,10 +3,13 @@
 
 /**
  * get_bit - extract the bit at position @index
+ * @n: number to extract the bit from
  * @index: the position of the bit to be extracted
  * Return: the extracted bit
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	if (index > 31)
+		return (-1);
 	return ((n >> index) & 1);
 }
