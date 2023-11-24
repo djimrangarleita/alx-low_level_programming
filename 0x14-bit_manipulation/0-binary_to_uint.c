388ee33f41e9ab6b9c1a4bd01eb80f52930e7737
Djimra NGARLEITA <dngarleita@gmail.com>
2023-11-24 10:50:45 +0300

    chore: convert base 2 to base 10

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+/**
+ * binary_to_uint - convert binary to unsigned int
+ * @b: char ptr to a bit
+ * Return: the converted int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0, i, len;
+
+	i = 0;
+	len = strlen(b);
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		num += ((b[i] - '0') * (pow(2, --len)));
+		i++;
+	}
+
+	return (num);
+}
