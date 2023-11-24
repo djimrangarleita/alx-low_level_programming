6b50e853d458bdb447e9f96fd3f9034866ab5784
Djimra NGARLEITA <dngarleita@gmail.com>
2023-11-24 12:48:45 +0300

    fix: use left shift instead of power

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,7 +18,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		num += ((b[i] - '0') * _pow(2, --len));
+		num += ((b[i] - '0') * (1 << --len));
 		i++;
 	}
 
@@ -40,21 +40,4 @@ int _strlen(const char *str)
 	}
 
 	return (i);
-}
-
-/**
- * _pow - raise a num to a power @p
- * @p: the power
- * @b: base number
- * Return: int val
- */
-int _pow(int b, int p)
-{
-	if (p == 0)
-		return (1);
-
-	if (b == 0)
-		return (0);
-
-	return (b * _pow(b, p - 1));
 }
---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * print_binary - convert num to binary and print it
+ * @n: int, the decimal num to be converted to bin
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int a = 1;
+
+	printf("Left shifted by 1: %d\n", a << 4);
+}
