744ceab708030a1a2ad764f00c8719b26280d673
Djimra NGARLEITA <dngarleita@gmail.com>
2023-11-24 11:21:27 +0300

    fix: remove string.h lib

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,7 @@
 #include "main.h"
-#include <string.h>
 #include <math.h>
+
+int _strlen(const char *str);
 
 /**
  * binary_to_uint - convert binary to unsigned int
@@ -12,7 +13,7 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0, i, len;
 
 	i = 0;
-	len = strlen(b);
+	len = _strlen(b);
 	while (b && b[i] != '\0')
 	{
 		if (b[i] != '0' && b[i] != '1')
@@ -22,4 +23,21 @@ unsigned int binary_to_uint(const char *b)
 	}
 
 	return (num);
+}
+
+/**
+ * _strlen - return length of a given string
+ * @str: ptr to the string
+ * Return: int, length of @str
+ */
+int _strlen(const char *str)
+{
+	int i = 0;
+
+	while (str && str[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
 }
