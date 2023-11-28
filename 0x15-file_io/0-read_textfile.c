de7ab55b040b7066f03c3ad0460a1e5fc413139e
Djimra NGARLEITA <dngarleita@gmail.com>
2023-11-28 05:18:02 +0300

    fix: print output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,38 +1,55 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
+size_t _strlen(char *str);
 /**
  * read_textfile - read a text file and output it's content to stdout
  * @filename: name of the file to read
  * @letters: number of chars to be written to stdout
  * Return: number of letters red and printed
- * 	0 if file cannot be opened or red
- * 	0 if @filename is null
- * 	0 if write fails or does not write the expected amount of bytes
+ *	0 if file cannot be opened or red
+ *	0 if @filename is null
+ *	0 if write fails or does not write the expected amount of bytes
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *fptr;
 	int nchar = 0, tmp = 0;
-	char ch;
+	char *ch = malloc(letters * sizeof(char));
+	int fd;
+	size_t len = 0;
 
-	fptr = fopen(filename, "r");
+	fd = open(filename, O_RDONLY);
 
-	if (!filename || !fptr)
+	if (!filename || fd < 0)
+		return (0);
+	tmp = read(fd, ch, letters);
+	if (tmp < 0)
 		return (0);
 
-	while (letters > 0 && ch != EOF)
-	{
-		ch = fgetc(fptr);
-		tmp = write(1, &ch, 1);
-		if (tmp == 1)
-			nchar += tmp;
-		else
-			return (0);
-		letters--;
-	}
-	fclose(fptr);
+	len = _strlen(ch);
+	if (len < letters)
+		letters = len;
+	nchar = write(1, ch, letters);
+
+	close(fd);
+	free(ch);
 
 	return (nchar);
+}
+
+/**
+ * _strlen - return the size of the @str
+ * @str: string ptr
+ * Return: size of str
+ */
+size_t _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	return (i);
 }
