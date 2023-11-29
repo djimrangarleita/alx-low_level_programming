2a201d7ab2bf27e884e6e22164d036875f326838
Djimra NGARLEITA <dngarleita@gmail.com>
2023-11-29 05:52:37 +0300

    fix: if condition for endianness

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -59,24 +59,25 @@ void print_elf(Elf64_Ehdr *elfh)
 {
 	printf("Magic:	 ");
 	for (int i = 0; i < EI_NIDENT; i++)
-		printf("%02x ", elf_header.e_ident[i]);
+		printf("%02x ", elfh->e_ident[i]);
 	printf("\nClass:				 ");
-	printf("%s\n", elf_header.e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" :
+	printf("%s\n", elfh->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" :
 			"ELF32");
 	printf("Data:				   ");
-	printf("%s\n", elf_header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's
-			complement,	  little endian" : "2's complement, big
-			endian");
+	if (elfh->e_ident[EI_DATA] == ELFDATA2LSB)
+		printf("%s\n", "2's complement, little endian");
+	else
+		printf("%s\n", "2's complement, big endian");
 	printf("Version:				   ");
-	printf("%u\n", elf_header.e_ident[EI_VERSION]);
+	printf("%u\n", elfh->e_ident[EI_VERSION]);
 	printf("OS/ABI:				   ");
-	printf("%u\n", elf_header.e_ident[EI_OSABI]);
+	printf("%u\n", elfh->e_ident[EI_OSABI]);
 	printf("ABI Version:			   ");
-	printf("%u\n", elf_header.e_ident[EI_ABIVERSION]);
+	printf("%u\n", elfh->e_ident[EI_ABIVERSION]);
 	printf("Type:				   ");
-	printf("%u\n", elf_header.e_type);
+	printf("%u\n", elfh->e_type);
 	printf("Entry point address:		   ");
-	printf("%#010x\n", (unsigned int)elf_header.e_entry);
+	printf("%#010x\n", (unsigned int)elfh->e_entry);
 }
 
 /**
