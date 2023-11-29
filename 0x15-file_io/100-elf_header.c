9fecbb3e20fd76451203cec9574875883e279b78
djimrangarleita <dngarleita@gmail.com>
2023-11-29 05:58:44 +0300

    new

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -59,26 +59,26 @@ void print_elf(Elf64_Ehdr *elfh)
 {
 	int i;
 
-	printf("Magic:	 ");
+	printf("Magic:	  ");
 	for (i = 0; i < EI_NIDENT; i++)
 		printf("%02x ", elfh->e_ident[i]);
-	printf("\nClass:				 ");
+	printf("\nClass:                             ");
 	printf("%s\n", elfh->e_ident[EI_CLASS] == ELFCLASS64 ? "ELF64" :
 			"ELF32");
-	printf("Data:				   ");
+	printf("Data:                              ");
 	if (elfh->e_ident[EI_DATA] == ELFDATA2LSB)
 		printf("%s\n", "2's complement, little endian");
 	else
 		printf("%s\n", "2's complement, big endian");
-	printf("Version:				   ");
+	printf("Version:                           ");
 	printf("%u\n", elfh->e_ident[EI_VERSION]);
-	printf("OS/ABI:				   ");
+	printf("OS/ABI:                            ");
 	printf("%u\n", elfh->e_ident[EI_OSABI]);
-	printf("ABI Version:			   ");
+	printf("ABI Version:                       ");
 	printf("%u\n", elfh->e_ident[EI_ABIVERSION]);
-	printf("Type:				   ");
+	printf("Type:                              ");
 	printf("%u\n", elfh->e_type);
-	printf("Entry point address:		   ");
+	printf("Entry point address:               ");
 	printf("%#010x\n", (unsigned int)elfh->e_entry);
 }
 
