3332826e007dbc49f3a1fa37a07722b9ac5b45c9
Djimra NGARLEITA <dngarleita@gmail.com>
2023-12-14 20:21:25 +0300

    refactor: improve code and remove std lib

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,17 @@
-#include <string.h>
+/**
+ * _strlen - calculate the length of the string @str
+ * @str: char pointer to string
+ * Return: int representing the length of @str
+ */
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (str && str[len])
+		len++;
+
+	return (len);
+}
 
 /**
  * _strcat - Concatenate 2 strings
@@ -8,5 +21,18 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	return (strcat(dest, src));
+	int i = 0, j = 0, destlen = 0, srclen = 0;
+
+	destlen = _strlen(dest);
+	srclen = _strlen(src);
+	while (dest[i] != '\0')
+		i++;
+
+	for (; i < destlen + srclen; i++)
+	{
+		dest[i] = src[j];
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
