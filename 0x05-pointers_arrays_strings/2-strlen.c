fda2d9c612440bec9f38a2a80401ab0390e58192
Djimra NGARLEITA <dngarleita@gmail.com>
2023-12-14 20:37:02 +0300

    refactor: rm string.h

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,14 @@
-#include <string.h>
-
 /**
- * _strlen - Takes a string param and return its length
- * @s: The string who's length must be returned
- * Return: Int, length of the string s
+ * _strlen - calculate the length of the string @str
+ * @str: char pointer to string
+ * Return: int representing the length of @str
  */
 int _strlen(char *s)
 {
-	return (strlen(s));
+	int len = 0;
+
+	while (s && s[len])
+		len++;
+
+	return (len);
 }
