1920456683374e4afda06526079c000bffd14432
Djimra NGARLEITA <dngarleita@gmail.com>
2024-01-12 07:52:57 +0300

    fix: split print in 2 lines

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,5 +6,6 @@ void first_exec(void) __attribute__((constructor));
  */
 void first_exec(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
