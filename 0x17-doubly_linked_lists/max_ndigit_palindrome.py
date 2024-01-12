eea386e7d0b3630ef68db759385fb791c43a94c1
Djimra NGARLEITA <dngarleita@gmail.com>
2024-01-12 03:26:54 +0300

    chore: largest 2 3-digits product palindrome

---
+++ 0x17-doubly_linked_lists/max_ndigit_palindrome.py
@@ -0,0 +1,19 @@
+def max_ndigit_palindrome(n):
+    highndigit_num = (10**n) - 1
+    minndigit_num = 1 + (highndigit_num // 10)
+    max_product = 0;
+
+    for i in range(highndigit_num, (minndigit_num - 1), -1):
+        for j in range(i, (minndigit_num - 1), -1):
+            product = i * j
+            if product < max_product:
+                break
+
+            if str(product) == str(product)[::-1] and product > max_product:
+                max_product = product
+
+    return max_product
+
+print(max_ndigit_palindrome(3))
+print(max_ndigit_palindrome(2))
+print(max_ndigit_palindrome(4))
