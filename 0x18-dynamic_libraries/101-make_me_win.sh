bb1fc85dd14063c7e5a8cfdd70b5557ac5ff4e86
Djimra NGARLEITA <dngarleita@gmail.com>
2024-01-16 01:51:16 +0300

    test: change dowload link to raw

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -qP /tmp https://github.com/djimrangarleita/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libgm.so
+wget -qLP /tmp https://github.com/djimrangarleita/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgm.so
 export LD_PRELOAD=/tmp/libgm.so
