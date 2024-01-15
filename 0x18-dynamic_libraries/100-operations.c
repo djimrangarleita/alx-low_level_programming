695222ad737003053316ecf308c1b1856c4954b5
Djimra NGARLEITA <dngarleita@gmail.com>
2024-01-15 20:37:03 +0300

    chore: create c lib for +, -, *, / and %

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,56 @@
+/**
+ * add - add two int a, b
+ * @a: first int
+ * @b: second int
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract b from a
+ * @a: first int
+ * @b: second int
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - mul int a and b
+ * @a: first int
+ * @b: second int
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divide a by b
+ * @a: first int
+ * @b: second int
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+	if (b != 0)
+		return (a / b);
+}
+
+
+/**
+ * mod - do a modulo b
+ * @a: first int
+ * @b: second int
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
