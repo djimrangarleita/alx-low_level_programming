d9a482a69454838c8e2b4c27b7a674fa52059a26
Djimra NGARLEITA <dngarleita@gmail.com>
2024-01-16 00:42:36 +0300

    fix: rm lbr

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-wget -qP /tmp
-https://github.com/djimrangarleita/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libgm.so
+wget -qP /tmp https://github.com/djimrangarleita/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libgm.so
 export LD_PRELOAD=/tmp/libgm.so
