c4a8ffa0004116fe25077297d9a18eff0b6cd42f
Djimra NGARLEITA <dngarleita@gmail.com>
2024-02-02 03:04:20 +0300

    Initial commit, static

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,18 @@
+/**
+ * hash_djb2 - hash function djb2 implementation
+ * @str: key to be hashed
+ * Return: hash of key
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+    unsigned long int hash;
+    int c;
+
+    hash = 5381;
+    while ((c = *str++))
+    {
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
+    return (hash);
+}
